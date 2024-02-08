8bdc7b80d4ca4752a56e404fe451f8e8391ac6a0
Marius Iordan <dopyno@yahoo.com>
2024-02-08 12:27:52 +0000

    events

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+//?   Selectarea si modificarea unui element html
+//*   document.querySelector('.class');                           //* selectare elementul html
+//*   document.querySelector('.class').textContent;               //* selectare doar textul din elementul html
+//*   document.querySelector('.class').textContent = 'textString'; // *string
+//*   document.querySelector('.class').textContent = 10;            // *value
+//*   document.querySelector('.class').value;
+//*   document.querySelector('.class').value = 10;    // *value modificarea valorii
+                                                     
+//*   document.getElementById('.idElement').innerHTML;
+//*   document.getElementById('#idElement').value = 10;
+//*   document.getElementById('idElement').innerHTML;
