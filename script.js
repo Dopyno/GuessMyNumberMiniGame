49af4f99e0f6e750fa7359fe4d1e31435f65f1b6
Marius Iordan <dopyno@yahoo.com>
2024-02-08 16:04:05 +0000

    up

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 'use strict';
 
 //?   Selectarea si modificarea unui element html
+
 //*   document.querySelector('.class');                           //* selectare elementul html
 //*   document.querySelector('.class').textContent;               //* selectare doar textul din elementul html
 //*   document.querySelector('.class').textContent = 'textString'; // *string
 //*   document.querySelector('.class').textContent = 10;            // *value
 //*   document.querySelector('.class').value;
 //*   document.querySelector('.class').value = 10;    // *value modificarea valorii
-                                                     
+
 //*   document.getElementById('.idElement').innerHTML;
 //*   document.getElementById('#idElement').value = 10;
 //*   document.getElementById('idElement').innerHTML;
+
+document.querySelector('#check-button').addEventListener('click', function () {
+  let randomElement = Number.parseInt(Math.random() * 21);
+  let temp = document.getElementById('user-input').value;
+  if (typeof temp !== 'number') {
+  }
+});
